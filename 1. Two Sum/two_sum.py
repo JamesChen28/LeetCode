d2f97ee4d1c123504bd9812727f6cace3c0758f3
JamesChen28 <james5520042000@gmail.com>
2023-05-13 01:51:14 +0800

    add #9 solution

---
+++ 1. Two Sum/two_sum.py
@@ -0,0 +1,32 @@
+
+## https://leetcode.com/problems/two-sum/
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        n = len(nums)
+        hash = {}
+        for i in range(n):
+            num1 = nums[i]
+            num2 = target - num1
+            if num2 in hash:
+                return [hash[num2], i]
+            hash[num1] = i
+
+
+## test
+Solution().twoSum(nums = [2,7,11,15], target = 9)
+Solution().twoSum(nums = [3,2,4], target = 6)
+Solution().twoSum(nums = [3,3], target = 6)
+
+
+## guidance
+# n1 + n2 = target <=> n2 = target - n1
+# use hash table to store n1 and its index information
+# if target - n1 in hash table, then target - n1 is the solution
+# the time complexity is O(n)
+# the space complexity is O(n)
---
+++ 9. Palindrome Number/is_palindrome.py
@@ -0,0 +1,29 @@
+
+# https://leetcode.com/problems/palindrome-number/
+
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x >= 0:
+            x = str(x)
+            if x == x[::-1]:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+## test
+Solution().isPalindrome(x = 121)
+Solution().isPalindrome(x = -121)
+Solution().isPalindrome(x = 10)
+
+
+## guidance
+# x[::-1] is the reverse string of string x
+# check x[::-1] equal to x or not
+# the time complexity is O(n)
+# the space complexity is O(1)
